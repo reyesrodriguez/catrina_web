32798362bc06b8005b70cec2183d7dc60efe4d90
reyes rodriguez rodriguez <reyesorodriguez@gmail.com>
2015-12-15 23:48:06 -0500

    first draft

--- public/javascripts/style.js
+++ public/javascripts/style.js
@@ -2,14 +2,23 @@ $( document ).ready(function() {
     console.log( "ready!" );
    
    
-   		$(function(){
+   		// $(function(){
 
-   		setTimeout(function () {
-        $('#myModal').modal('toggle');
-    }, 2000);
+   	//	setTimeout(function () {
+    //    $('#myModal').modal('toggle');
+    //}, 2000);
    		
 
-   })
+   //})
+
+   	$('#about').on('click', function(e){
+    e.preventDefault();
+    var target= $(this).get(0).id == 'about' ? $('#down') : $('#up');
+    $('html, body').stop().animate({
+       scrollTop: target.offset().top
+    }, 1000);
+});
+
 });
     
 
